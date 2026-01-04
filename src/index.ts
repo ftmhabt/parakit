a442cf44c3356da01273fea92c4f955412b96862
ftmhabt <ftmh.a78@gmail.com>
2026-01-04 18:31:31 +0330

    Create entry point

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// Providers
+export { ParakitProvider } from "./providers/ParakitProvider";
+
+// Components
+export { Button } from "./components/Button";
+
+// Hooks
+export { useParakit } from "./context/ParakitContext";
+export { usePhysicsSpring } from "./hooks/usePhysicsSpring";
+export { useSound } from "./hooks/useSound";
+
+// Types
+export type {
+  ComponentSize,
+  ParakitConfig,
+  PhysicsConfig,
+  SoundName,
+} from "./types";
